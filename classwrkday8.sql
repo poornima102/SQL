2c1d90eee3c9c734865e2c2a55a187417d7ba219
poornima <poornimachandran102@gmail.com>
2025-12-15 12:20:10 +0530

    classwrkday8

---
+++ classwrkday8.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE GroceryShop;
+USE GroceryShop;
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    price DECIMAL(10,2)
+);
+ALTER TABLE products
+ADD category VARCHAR(50);
+DELETE FROM products;
+DROP DATABASE GroceryShop;
